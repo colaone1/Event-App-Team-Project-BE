02b08150ecab231575b890b7131b8b0a1b25165f
JDC-droid <josh040890@gmail.com>
2025-05-28 10:34:18 +0100

    Add eventController and eventAuth

---
+++ controllers/eventController.js
@@ -0,0 +1,42 @@
+const Event = require("../Models/Event");
+const User = require("../Models/User");
+
+exports.getEvents = async (req, res) => {
+  try {
+    const events = await Event.find();
+    res.json(events);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.addEvent = async (req, res) => {
+  const userToken = req.headers.authorization.split(" ")[1];
+
+  console.log(userToken);
+
+  if (!userToken) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const userInDB = await User.findOne({ token: userToken });
+
+  console.log(userInDB);
+
+  if (!userInDB) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const event = new Event({
+    title: req.body.title,
+    description: req.body.description,
+    price: req.body.price,
+    userId: userInDB._id,
+  });
+  try {
+    const newEvent = await event.save();
+    res.status(201).json(newEvent);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
